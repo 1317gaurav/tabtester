e8573f2ed8f83d5537394efc2f2a98d0cb314683
Gaurav Kumar <gs1465010@gmail.com>
2020-01-24 17:04:50 +0530

    Update config.php

--- src/feedback/config.php
+++ src/feedback/config.php
@@ -7,6 +7,6 @@ $client->addScope(Google_Service_Oauth2::USERINFO_PROFILE);
 $client->addScope(Google_Service_Oauth2::USERINFO_EMAIL);
 $client->addScope(Google_Service_Oauth2::USERINFO_EMAIL);
 $client->addScope(Google_Service_Oauth2::PLUS_ME);
-$client->setRedirectUri("http://brahmarishisaubhari.com/feedback/");
+$client->setRedirectUri("http://ps001.taboolasyndication.com:8999/feedback/");
 
 ?>
