0d8f206611b3581e5dc410fcfb51917f9caca8b8
Gaurav Kumar <gs1465010@gmail.com>
2020-01-25 20:06:48 +0530

    Update logout.php

--- src/feedback/Feedback/logout.php
+++ src/feedback/Feedback/logout.php
@@ -2,6 +2,6 @@
 session_start(); //to ensure you are using same session
 unset($_SESSION);
 session_destroy(); //destroy the session
-header("location:../../src/"); //to redirect back to "index.php" after logging out
+header("location:../../src/index.html"); //to redirect back to "index.php" after logging out
 exit();
 ?>
