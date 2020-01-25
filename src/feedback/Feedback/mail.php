a500f7d22405ae78759efc4ef8091a1424abe3f8
Gaurav Kumar <gs1465010@gmail.com>
2020-01-25 19:32:26 +0530

    Update mail.php

--- src/feedback/Feedback/mail.php
+++ src/feedback/Feedback/mail.php
@@ -76,6 +76,22 @@ $domain1="@taboola.com";
 if($domain == $domain1)
 {
     mail("$to", $subject, $message, $headers);
+    
+    
+    
+    $msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("gaurav.k@taboola.com","My subject",$msg);
+    
+    
+    
+    
+    
+    
  $message = '';  
  $error = '';  
   
