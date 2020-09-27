b78e9faad8c97b93f03f3c2a311eadc0d0f1eee0
vishal-taboola <47807107+vishal-taboola@users.noreply.github.com>
2020-09-27 21:38:16 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ docker stop tab
 docker container prune -f
 # docker rmi tsocial
 # docker image prune -a -f
-docker build .  -t tab
+# docker build .  -t tab
 docker build .  -t tab --network host
 #docker-compose up -d 
 docker run  -p 80:555 -d --name tab  tab:latest
