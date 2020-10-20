506e59ae5d248218d4dea056b819dac8ee780443
Gaurav Kumar <gs1465010@gmail.com>
2020-10-20 17:08:03 +0530

    Update index.php

--- src/feedback/Feedback/index.php
+++ src/feedback/Feedback/index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if(!isset($_SESSION['user']['email']))
-{
-header("Location:../../index.html");    
+// {
+// header("Location:../../index.html");    
     
-}
+// }
 ?>
 <html>
 <head>
