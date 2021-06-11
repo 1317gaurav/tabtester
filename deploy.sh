f0782a363972bb9d1ba7b68f2845aa8d37aca7c6
Gaurav Kumar <gs1465010@gmail.com>
2021-06-11 17:26:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-docker stop tabtester
-docker container prune -f
+sudo docker stop tabtester
+sudo docker container prune -f
 # docker rmi tsocial
 # docker image prune -a -f
 # docker build .  -t tab
-docker build .  -t tabtester --network host
-docker-compose up -d 
+sudo docker build .  -t tabtester --network host
+sudo docker-compose up -d 
 # docker run  -p 80:443 -d --name tab  tab:latest
